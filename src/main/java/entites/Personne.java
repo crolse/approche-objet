358ed1a993a4ac353d5eb1c8655775850e1831cb
GregoireEpsi <gregoire.barre@epsi.fr>
2020-10-28 14:02:58 +0100

    1

--- src/main/java/entites/Personne.java
+++ src/main/java/entites/Personne.java
@@ -1,9 +1,9 @@
 package entites;
 
 public class Personne {
-    String nom ;
-    String prenom ;
-    String adressePostale ;
+    static String nom ;
+    static String prenom ;
+    static String adressePostale ;
 
     public Personne(String nom, String prenom){
         this.nom = nom ;
@@ -14,5 +14,44 @@ public class Personne {
         this.nom = nom ;
         this.prenom = prenom ;
         this.adressePostale = adressePostale ;
+    }
+
+    public static void nomFamilleMajuscule (String nom , String prenom) {
+        System.out.println(prenom + nom.toUpperCase());
+    }
+
+    public static void setNom (String nomNouveau) {
+        nom = nomNouveau ;
+
+    }
+
+    public static void setPrenom (String prenomNouveau) {
+        prenom = prenomNouveau ;
+
+    }
+
+    public static void setAdresse (String adresseNouveau) {
+        adressePostale = adresseNouveau ;
+
+    }
+
+    public static String getNom () {
+        return nom ;
+
+    }
+
+    public static String getPrenom () {
+        return prenom ;
+
+    }
+    public static String getAdresse () {
+        return adressePostale ;
+
+    }
+
+
+    public static void main(String[] args) {
+        Personne personneA = new Personne("barre" ,"gregoire" , "44190") ;
+        nomFamilleMajuscule("barre" , "gregoire");
     }
 }
--- src/main/java/essais/TestPersonne.java
+++ src/main/java/essais/TestPersonne.java
@@ -6,6 +6,8 @@ public class TestPersonne {
     public static void main(String[] args) {
         Personne personneA = new Personne("pas" , "moi" ,"44190") ;
         Personne personneB = new Personne("bibou" , "blop" , "44190") ;
+        personneA.setAdresse("4400");
+        System.out.println(personneA.getAdresse());
     }
 
 }
